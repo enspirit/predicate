f8a4821ed6a69640bfdf5509abc29a4d2ff1e64c
Bernard Lambeau <blambeau@gmail.com>
2018-05-26 10:12:39 +0200

    Make Sequel compilation methods reusable in another Sexpr processor.
    For cases where Predicate users need to hack a little bit on larger
    expressions containing predicates, it is handy to actually have
    the compiler methods reusable as a module.

--- lib/predicate/sequel/to_sequel.rb
+++ lib/predicate/sequel/to_sequel.rb
@@ -1,84 +1,85 @@
 class Predicate
   class ToSequel < Sexpr::Processor
-
-    def on_identifier(sexpr)
-      ::Sequel.identifier(sexpr.last)
-    end
-
-    def on_qualified_identifier(sexpr)
-      ::Sequel.identifier(sexpr.name).qualify(sexpr.qualifier)
-    end
-
-    def on_literal(sexpr)
-      sexpr.last.nil? ? nil : ::Sequel.expr(sexpr.last)
-    end
-
-    ###
-
-    def on_tautology(sexpr)
-      ::Sequel::SQL::BooleanConstant.new(true)
-    end
-
-    def on_contradiction(sexpr)
-      ::Sequel::SQL::BooleanConstant.new(false)
-    end
-
-    def on_eq(sexpr)
-      left, right = apply(sexpr.left), apply(sexpr.right)
-      ::Sequel.expr(left => right)
-    end
-
-    def on_neq(sexpr)
-      left, right = apply(sexpr.left), apply(sexpr.right)
-      ~::Sequel.expr(left => right)
-    end
-
-    def on_dyadic_comp(sexpr)
-      left, right = apply(sexpr.left), apply(sexpr.right)
-      left.send(sexpr.operator_symbol, right)
-    end
-    alias :on_lt  :on_dyadic_comp
-    alias :on_lte :on_dyadic_comp
-    alias :on_gt  :on_dyadic_comp
-    alias :on_gte :on_dyadic_comp
-
-    def on_in(sexpr)
-      left, right = apply(sexpr.identifier), sexpr.last
-      right = [right] if !right.is_a?(Array) && right.respond_to?(:sql_literal)
-      ::Sequel.expr(left => right)
-    end
-
-    def on_not(sexpr)
-      ~apply(sexpr.last)
-    end
-
-    def on_and(sexpr)
-      body = sexpr.sexpr_body
-      body[1..-1].inject(apply(body.first)){|f,t| f & apply(t) }
-    end
-
-    def on_or(sexpr)
-      body = sexpr.sexpr_body
-      body[1..-1].inject(apply(body.first)){|f,t| f | apply(t) }
-    end
-
-    def on_match(sexpr)
-      left, right = sexpr.left, sexpr.right
-      left  = [ left.first,  "%#{left.last}%"  ] if left.first  == :literal && !left.last.is_a?(Regexp)
-      right = [ right.first, "%#{right.last}%" ] if right.first == :literal && !right.last.is_a?(Regexp)
-      left, right = apply(left), apply(right)
-      if sexpr.case_sentitive?
-        left.like(right)
-      else
-        left.ilike(right)
+    module Methods
+      def on_identifier(sexpr)
+        ::Sequel.identifier(sexpr.last)
       end
-    end
 
-    def on_unsupported(sexpr)
-      raise NotSupportedError
-    end
-    alias :on_native :on_unsupported
-    alias :on_intersect :on_unsupported
+      def on_qualified_identifier(sexpr)
+        ::Sequel.identifier(sexpr.name).qualify(sexpr.qualifier)
+      end
 
+      def on_literal(sexpr)
+        sexpr.last.nil? ? nil : ::Sequel.expr(sexpr.last)
+      end
+
+      ###
+
+      def on_tautology(sexpr)
+        ::Sequel::SQL::BooleanConstant.new(true)
+      end
+
+      def on_contradiction(sexpr)
+        ::Sequel::SQL::BooleanConstant.new(false)
+      end
+
+      def on_eq(sexpr)
+        left, right = apply(sexpr.left), apply(sexpr.right)
+        ::Sequel.expr(left => right)
+      end
+
+      def on_neq(sexpr)
+        left, right = apply(sexpr.left), apply(sexpr.right)
+        ~::Sequel.expr(left => right)
+      end
+
+      def on_dyadic_comp(sexpr)
+        left, right = apply(sexpr.left), apply(sexpr.right)
+        left.send(sexpr.operator_symbol, right)
+      end
+      alias :on_lt  :on_dyadic_comp
+      alias :on_lte :on_dyadic_comp
+      alias :on_gt  :on_dyadic_comp
+      alias :on_gte :on_dyadic_comp
+
+      def on_in(sexpr)
+        left, right = apply(sexpr.identifier), sexpr.last
+        right = [right] if !right.is_a?(Array) && right.respond_to?(:sql_literal)
+        ::Sequel.expr(left => right)
+      end
+
+      def on_not(sexpr)
+        ~apply(sexpr.last)
+      end
+
+      def on_and(sexpr)
+        body = sexpr.sexpr_body
+        body[1..-1].inject(apply(body.first)){|f,t| f & apply(t) }
+      end
+
+      def on_or(sexpr)
+        body = sexpr.sexpr_body
+        body[1..-1].inject(apply(body.first)){|f,t| f | apply(t) }
+      end
+
+      def on_match(sexpr)
+        left, right = sexpr.left, sexpr.right
+        left  = [ left.first,  "%#{left.last}%"  ] if left.first  == :literal && !left.last.is_a?(Regexp)
+        right = [ right.first, "%#{right.last}%" ] if right.first == :literal && !right.last.is_a?(Regexp)
+        left, right = apply(left), apply(right)
+        if sexpr.case_sentitive?
+          left.like(right)
+        else
+          left.ilike(right)
+        end
+      end
+
+      def on_unsupported(sexpr)
+        raise NotSupportedError
+      end
+      alias :on_native :on_unsupported
+      alias :on_intersect :on_unsupported
+    end
+    include Methods
   end # class ToSequel
 end # class Predicate
